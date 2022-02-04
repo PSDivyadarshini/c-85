2bb9b123c0ae779bf4ee86448969fe127ba061a2
PSDivyadarshini <74144949+PSDivyadarshini@users.noreply.github.com>
2022-02-04 18:05:40 +0530

    Add files via upload
    c-85

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB3htPJDeGBLdKYZWRyHlb4Cz5q_gXdlpM",
+    authDomain: "storyapp-c77ba.firebaseapp.com",
+    projectId: "storyapp-c77ba",
+    storageBucket: "storyapp-c77ba.appspot.com",
+    messagingSenderId: "918379280981",
+    appId: "1:918379280981:web:ba20c5dc148433165972dc"
+  };
